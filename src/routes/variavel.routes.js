31106171b68395c0c79db898a68d0a0f9787795d
Gabriel-Azevedo-Batalha <batalha800@gmail.com>
2022-07-30 15:19:35 -0300

    Adding variable requests

--- src/routes/variavel.routes.js
+++ src/routes/variavel.routes.js
@@ -5,5 +5,7 @@ const variavelRouter = express.Router();
 const VariavelControler = require('../controller/VariavelController');
 
 variavelRouter.post("/", VariavelControler.createVariavel);
+variavelRouter.post("/2", VariavelControler.requestVariavel);
+variavelRouter.post("/3", VariavelControler.requestVariavelFiltered);
 
 module.exports = variavelRouter;
