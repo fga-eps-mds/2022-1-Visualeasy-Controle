70cb8e9beca2f5527b99d6bc6de53b4635eb4eaa
marcos-mv <vinicius_kof36@hotmail.com>
2022-08-01 00:11:53 -0300

    Changing data type from the variable date on postgres
    Co-authored-by: damarcones <damarcones@gmail.com>
    Co-authored-by: Gabriel-Azevedo-Batalha <batalha800@gmail.com>

--- postgresScripts/variavel-schema.sql
+++ postgresScripts/variavel-schema.sql
@@ -2,7 +2,7 @@ CREATE SCHEMA variavel;
 CREATE TABLE variavel.variavels (
  "id" SERIAL NOT NULL ,
  "variavel" varchar(20) NOT NULL,
- "data" date NOT NULL,
+ "data" timestamp NOT NULL,
  "valor" numeric NOT NULL,
  CONSTRAINT "Variavels_pk" PRIMARY KEY ("id")
 );
