ca2e51682a0ca26e26b98af14efd5022b1a0bce4
Gabriel-Azevedo-Batalha <batalha800@gmail.com>
2022-08-08 20:49:23 -0300

    Changed request routes
    Co-authored-by: damarcones <damarcones@gmail.com>

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,9 @@ const express = require('express');
 const routes = express.Router();
 
 const variavelRouter = require('./variavel.routes');
+const requestRouter = require('./request.routes');
 
 routes.use('/createVariable', variavelRouter);
+routes.use('/request', requestRouter);
 
 module.exports = routes;
--- src/routes/variavel.routes.js
+++ src/routes/variavel.routes.js
@@ -5,7 +5,5 @@ const variavelRouter = express.Router();
 const VariavelControler = require('../controller/VariavelController');
 
 variavelRouter.post("/", VariavelControler.createVariavel);
-variavelRouter.post("/2", VariavelControler.requestVariavel);
-variavelRouter.post("/3", VariavelControler.requestVariavelFiltered);
 
 module.exports = variavelRouter;
