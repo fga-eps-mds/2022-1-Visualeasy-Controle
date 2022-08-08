4bb6d055bbc6770bec8ac7fb68c70f03b7351c6e
Gabriel-Azevedo-Batalha <batalha800@gmail.com>
2022-08-08 20:25:40 -0300

    Changing VariavelRequestByName select to be case-insensitive
    Co-authored-by: damarcones <damarcones@gmail.com>

--- src/controller/VariavelRequest.js
+++ src/controller/VariavelRequest.js
@@ -2,26 +2,17 @@ const Variavel = require('../models/variavel');
 const Sequelize = require('sequelize');
 
 module.exports = {
-
-    /*async VariavelRequestUniques() {
-        const variavels = Variavel.findAll({attributes: 
-              [[Sequelize.fn('DISTINCT', Sequelize.col('variavel')), 'variavel']]
-          });
-        return variavels;
-    },*/
-
     async VariavelRequestByName(request) {
         const variavel = "%" + request.body.variavel + "%";
         const variavels = Variavel.findAll({
             attributes: 
               [[Sequelize.fn('DISTINCT', Sequelize.col('variavel')), 'variavel']], 
             where: {
-                variavel: {[Sequelize.Op.like]: variavel}
+                variavel: {[Sequelize.Op.iLike]: variavel}
               }
         });
         return variavels;
     },
-
     async VariavelRequestFiltered(request) {
         const {variavel, startDate, endDate} = request.body;
         const variavels = Variavel.findAll({where: {
@@ -31,5 +22,4 @@ module.exports = {
         });
         return variavels;
     }
-
 }
