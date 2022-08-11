225efc98c293160c43a7d21bcf088f282af22ab1
marcos-mv <vinicius_kof36@hotmail.com>
2022-08-11 17:34:31 -0300

    Tratando erros em VariavelCreate.
    Co-authored-by: brunocmo <brunocmo.nunes@gmail.com>

--- src/controller/VariavelCreate.js
+++ src/controller/VariavelCreate.js
@@ -1,9 +1,13 @@
 const Variavel = require('../models/variavel');
 
 module.exports = {
-  async VariavelCreate(request) {
-    const { id, variavel, data, valor } = request.body;
-    const newVariavel = Variavel.create({ id, variavel, data, valor });
-    return newVariavel;
-  },
+    async VariavelCreate(request) {
+        try {
+            const { id, variavel, data, valor } = request.body;
+            const newVariavel = Variavel.create({ id, variavel, data, valor });
+            return newVariavel;
+        } catch (err) {
+            return response.status(404).json(err);
+        }
+    },
 };
