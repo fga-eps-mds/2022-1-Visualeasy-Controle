7938bfede013b980c28f15fb1a4490db1a910895
Bruno Carmo Nunes <brunocmo.nunes@gmail.com>
2022-08-30 23:40:21 -0300

    Test default values for options
    Co-authored-by: marcos-mv <vinicius_kof36@hotmail.com>

--- src/app.js
+++ src/app.js
@@ -9,8 +9,10 @@ const Sequelize = require('sequelize');
 let app = express();
 
 let corsOptions = {
-    origin: 'http://localhost:3000',
-    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204
 };
 
 app.use(helmet.hidePoweredBy());
