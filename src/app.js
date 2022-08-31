54c2a9767eaaa2961a36b8a899280f014d63f755
Bruno Carmo Nunes <brunocmo.nunes@gmail.com>
2022-08-31 01:18:11 -0300

    List of options at routes
    Co-authored-by: marcos-mv <vinicius_kof36@hotmail.com>

--- src/app.js
+++ src/app.js
@@ -8,25 +8,7 @@ const Sequelize = require('sequelize');
 
 let app = express();
 
-let allowlist = [
-  'https://visualeasy-controle.herokuapp.com/',
-  'https://visualeasy-controle.herokuapp.com/variavel/create',
-  'https://visualeasy-controle.herokuapp.com/variavel/getNamesByName',
-  'https://visualeasy-controle.herokuapp.com/variavel/filtered',
-  'https://visualeasy-controle.herokuapp.com/variavel/filteredByPeriod',
-]
-let corsOptionsDelegate = function (req, callback) {
-  let corsOptions;
-  if (allowlist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-  } else {
-    corsOptions = { origin: false } // disable CORS for this request
-  }
-  callback(null, corsOptions) // callback expects two parameters: error and options
-}
-
 app.use(helmet.hidePoweredBy());
-app.use(cors(corsOptionsDelegate));
 app.use(bodyParser.json());
 app.use(routes);
 if (process.env.NODE_ENV == "development") {
--- src/routes/variavel.routes.js
+++ src/routes/variavel.routes.js
@@ -1,14 +1,33 @@
 const express = require('express');
+const cors = require('cors');
 
 const variavelRouter = express.Router();
 
 const VariavelControler = require('../controller/VariavelController');
 
-variavelRouter.get('/', VariavelControler.requestAllVariableNames);
+let allowlist = [
+  'https://visualeasy-controle.herokuapp.com/variavel/',
+  'https://visualeasy-controle.herokuapp.com/variavel/create',
+  'https://visualeasy-controle.herokuapp.com/variavel/getNamesByName',
+  'https://visualeasy-controle.herokuapp.com/variavel/filtered',
+  'https://visualeasy-controle.herokuapp.com/variavel/filteredByPeriod',
+]
 
-variavelRouter.post('/create', VariavelControler.createVariavel);
-variavelRouter.post('/getNamesByName', VariavelControler.requestVariavelByName); // request/variableNames
-variavelRouter.post('/filtered', VariavelControler.requestVariavelFiltered); // request/variables
-variavelRouter.post('/filteredByPeriod', VariavelControler.requestVariavelFilteredByFixedPeriod);
+function corsOptionsDelegate (number) {
+  let corsOptions = {
+
+    origin: allowlist[number],
+    optionsSuccessStatus: 204, // some legacy browsers (IE11, various SmartTVs) choke on 204 
+  }
+
+  return corsOptions;
+}
+
+variavelRouter.get('/', cors(corsOptionsDelegate(0)) ,VariavelControler.requestAllVariableNames);
+
+variavelRouter.post('/create', cors(corsOptionsDelegate(1)) ,VariavelControler.createVariavel);
+variavelRouter.post('/getNamesByName', cors(corsOptionsDelegate(2)) ,VariavelControler.requestVariavelByName); // request/variableNames
+variavelRouter.post('/filtered', cors(corsOptionsDelegate(3)) ,VariavelControler.requestVariavelFiltered); // request/variables
+variavelRouter.post('/filteredByPeriod', cors(corsOptionsDelegate(4)), VariavelControler.requestVariavelFilteredByFixedPeriod);
 
 module.exports = variavelRouter;
